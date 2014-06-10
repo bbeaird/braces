890ab32384a5db0889bd62dbe4749a7a3ef99d40
Brantley Beaird <bbeaird@gmail.com>
2014-06-10 16:56:22 -0700

    Refactored to produce a function check_for_equal_opening_and_closing_braces.

--- braces.rb
+++ braces.rb
@@ -11,15 +11,17 @@ def braces(array_of_braces_strings)
     split_str.each do |char|
       braces_hash[char] += 1
     end
-
-    if braces_hash['('] == braces_hash[')'] && braces_hash['['] == braces_hash[']'] && braces_hash['{'] == braces_hash['}']
-      puts 1
-    else
-      puts 0
-    end
-
+    check_for_equal_opening_and_closing_braces(braces_hash)
   end
   puts "Done!"
+end
+
+def check_for_equal_opening_and_closing_braces(braces_hash)
+  if braces_hash['('] == braces_hash[')'] && braces_hash['['] == braces_hash[']'] && braces_hash['{'] == braces_hash['}']
+    puts 1
+  else
+    puts 0
+  end
 end
 
 test = ")(){}","[]({})","([])","{()[]}","([)]"
