020840a7e7c393cf397f226e213e820344b1e83c
Brantley Beaird <bbeaird@gmail.com>
2014-06-10 20:54:29 -0700

    Refactored iterative solution.

--- braces.rb
+++ braces.rb
@@ -1,8 +1,12 @@
+OPENER_AND_CLOSER_PAIRS = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}'
+}.freeze
+
+# iterative solution
 def braces(array_of_braces_strings)
   array_of_braces_strings.each do |str|
-    split_str = str.split('')
-    deepest_level = []
-    $opener_and_closer_pairs = { '(' => ')', '[' => ']', '{' => '}' }
     $passing_flag = 1
     braces_hash = {
       '(' => 0,
@@ -12,19 +16,30 @@ def braces(array_of_braces_strings)
       '{' => 0,
       '}' => 0
     }
-
-    split_str.each do |char|
-      deal_with_level(char, deepest_level)
-      increment_braces_hash(char, braces_hash)
-    end
-    check_braces_hash_for_equal_opening_and_closing_braces(braces_hash)
+    process_string(str.split(''), braces_hash)
   puts "#{$passing_flag}"
   end
 end
 
-def deal_with_level(char, deepest_level)
-  if $opener_and_closer_pairs.has_key?(char)
-    deepest_level << $opener_and_closer_pairs[char]
+def process_string(split_str, braces_hash, deepest_level=[])
+  braces_hash = {
+    '(' => 0,
+    ')' => 0,
+    '[' => 0,
+    ']' => 0,
+    '{' => 0,
+    '}' => 0
+  }
+  split_str.each do |char|
+    process_level(char, deepest_level)
+    increment_braces_hash(char, braces_hash)
+  end
+  check_braces_hash_for_equal_opening_and_closing_braces(braces_hash)
+end
+
+def process_level(char, deepest_level)
+  if OPENER_AND_CLOSER_PAIRS.has_key?(char)
+    deepest_level << OPENER_AND_CLOSER_PAIRS[char]
   elsif deepest_level.pop != char
     $passing_flag = 0
   end
@@ -35,19 +50,18 @@ def increment_braces_hash(char, braces_hash)
 end
 
 def check_braces_hash_for_equal_opening_and_closing_braces(braces_hash)
-  unless braces_hash['('] == braces_hash[')'] && braces_hash['['] == braces_hash[']'] && braces_hash['{'] == braces_hash['}']
+  unless (
+    braces_hash['('] == braces_hash[')'] &&
+    braces_hash['['] == braces_hash[']'] &&
+    braces_hash['{'] == braces_hash['}']
+  )
     $passing_flag = 0
   end
 end
 
 
 
-OPENER_AND_CLOSER_PAIRS = {
-  '(' => ')',
-  '[' => ']',
-  '{' => '}'
-}.freeze
-
+# recursive solution
 def braces_recursive(array_of_braces_strings)
   array_of_braces_strings.each do |str|
     recurse_each_string(str.split(''))
@@ -71,10 +85,12 @@ def end_of_string?(char, deepest_level)
 end
 
 def open_brace?(char)
-  OPENER_AND_CLOSER_PAIRS.keys.include? char
+  OPENER_AND_CLOSER_PAIRS.keys.include?(char)
 end
 
 
 test = ")(){}","[]({})","([])","{()[]}","([)]", "(", "((())"
+p "Iterative"
 braces(test)
+p "Recursive"
 braces_recursive(test)
