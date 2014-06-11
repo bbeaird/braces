6fac497f319c20698de3b3860e7a2c0b42e69608
Brantley Beaird <bbeaird@gmail.com>
2014-06-10 17:51:26 -0700

    Refactored. We check that closing brace is of the deepest level and that the number of opening and closing braces is the same.

--- braces.rb
+++ braces.rb
@@ -1,28 +1,44 @@
 def braces(array_of_braces_strings)
   array_of_braces_strings.each do |str|
     split_str = str.split('')
-    braces_hash = { '(' => 0,
-                    ')' => 0,
-                    '[' => 0,
-                    ']' => 0,
-                    '{' => 0,
-                    '}' => 0 }
+    deepest_level = []
+    $opener_and_closer_pairs = { '(' => ')', '[' => ']', '{' => '}' }
+    $passing_flag = 1
+    braces_hash = {
+      '(' => 0,
+      ')' => 0,
+      '[' => 0,
+      ']' => 0,
+      '{' => 0,
+      '}' => 0
+    }
 
     split_str.each do |char|
-      braces_hash[char] += 1
+      deal_with_level(char, deepest_level)
+      increment_braces_hash(char, braces_hash)
     end
-    check_for_equal_opening_and_closing_braces(braces_hash)
-  end
-  puts "Done!"
-end
-
-def check_for_equal_opening_and_closing_braces(braces_hash)
-  if braces_hash['('] == braces_hash[')'] && braces_hash['['] == braces_hash[']'] && braces_hash['{'] == braces_hash['}']
-    puts 1
-  else
-    puts 0
+    check_braces_hash_for_equal_opening_and_closing_braces(braces_hash)
+  puts "#{$passing_flag}"
   end
 end
 
-test = ")(){}","[]({})","([])","{()[]}","([)]"
+def deal_with_level(char, deepest_level)
+  if $opener_and_closer_pairs.has_key?(char)
+    deepest_level << $opener_and_closer_pairs[char]
+  elsif deepest_level.pop != char
+    $passing_flag = 0
+  end
+end
+
+def increment_braces_hash(char, braces_hash)
+  braces_hash[char] += 1
+end
+
+def check_braces_hash_for_equal_opening_and_closing_braces(braces_hash)
+  unless braces_hash['('] == braces_hash[')'] && braces_hash['['] == braces_hash[']'] && braces_hash['{'] == braces_hash['}']
+    $passing_flag = 0
+  end
+end
+
+test = ")(){}","[]({})","([])","{()[]}","([)]", "(", "((())"
 braces(test)
